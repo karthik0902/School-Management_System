0a942f107a9b91acf156a285721ae110837b772f
karthik <rangalakarthik@gmail.com>
2024-07-09 13:04:35 +0530

    update

--- frontend/src/Pages/Getstarted.js
+++ frontend/src/Pages/Getstarted.js
@@ -1,9 +1,6 @@
-
-
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import classes from "./Getstarted.css"
-import Stack from '@mui/material/Stack';
+import className from "./Getstarted.css"
 import Button from '@mui/material/Button';
 
 
@@ -12,9 +9,9 @@ const GetStartedPage = () => {
     return (
         <div className="get-started-page">
             
-            <div className="button-container">
+            <div className="Heading-button">
             <h1>Welcome to School Management System</h1>
-            <Button style={{marginLeft:"17%", marginTop:"10px",color:"rgb(35, 165, 208)" , height:"50px"}} onClick={()=>nav('/choose')} variant="outlined">Lets get started</Button>
+            <Button id='heading-btn' onClick={()=>nav('/choose')} variant="outlined">Lets get started</Button>
       
             </div>
             <img className='img1' src="https://img.freepik.com/free-vector/school-building-nature-scene_1308-29206.jpg?w=2000&t=st=1713846630~exp=1713847230~hmac=75b29615980f3ac9b640db0d7ec79d215eb14b52b950442c4829de6edaf22047" alt='school' />
--- frontend/src/Pages/choose.js
+++ frontend/src/Pages/choose.js
@@ -1,5 +1,3 @@
-
-
 import React from 'react';
 import './choose.css'; 
 import { useNavigate } from 'react-router-dom';
@@ -15,13 +13,13 @@ const ChooseLoginTypePage = () => {
             <h1>Choose Your Login</h1>
             <div className="login-options" style={{display:'flex'}}>
                 <button onClick={()=>{nav("/student")}} className="login-button student" >
-                    <img className='img' src={studentImageSrc}/>Student</button>
+                    <img className='choose-img' alt='Student' src={studentImageSrc}/>Student</button>
                 <button onClick={()=>{nav("/teacher")}} className="login-button teacher">
-                <img className='img' src={studentImageSrc4}/>Teacher</button>
+                <img className='choose-img' alt='Teacher' src={studentImageSrc4}/>Teacher</button>
                 <button onClick={()=>{nav("/Admin")}} className="login-button administrator">
-                <img className='img' src={studentImageSrc2}/>Administrator</button>
+                <img className='choose-img' alt='Administrator' src={studentImageSrc2}/>Administrator</button>
                 <button onClick={()=>{nav("/Parent")}} className="login-button parent">
-                <img className='img' src={studentImageSrc3}/>Parent</button>
+                <img className='choose-img' alt='Parent' src={studentImageSrc3}/>Parent</button>
             </div>
         </div>
     );
