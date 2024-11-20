b8072403e63590a6696d1a05eb7168b5f1b4f83d
karthik <rangalakarthik@gmail.com>
2024-11-20 16:01:11 +0530

    update

--- frontend/src/Pages/Getstarted.js
+++ frontend/src/Pages/Getstarted.js
@@ -14,7 +14,6 @@ const GetStartedPage = () => {
             <Button id='heading-btn' onClick={()=>nav('/choose')} variant="outlined">Lets get started</Button>
       
             </div>
-            <img className='img1' src="https://img.freepik.com/free-vector/school-building-nature-scene_1308-29206.jpg?w=2000&t=st=1713846630~exp=1713847230~hmac=75b29615980f3ac9b640db0d7ec79d215eb14b52b950442c4829de6edaf22047" alt='school' />
             
            
         </div>
