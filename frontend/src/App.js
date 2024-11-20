23a757662e035f6d36a0c3e4e2aa85c299a6eea3
karthik <rangalakarthik@gmail.com>
2024-11-20 16:01:28 +0530

    update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import './App.css';
+import "./App.css";
 import {  useState,useEffect } from "react";
 import { useNavigate } from 'react-router-dom';
 import { Routes, Route } from 'react-router-dom';
